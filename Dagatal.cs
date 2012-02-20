6469e3641c653c285708030f0e646c46bffee0ed
Heimir Þór Kjartansson <hemmi.hk@gmail.com>
2012-02-20 13:24:38 +0000

    Virkar!!

--- Dagatal.cs
+++ Dagatal.cs
@@ -1,58 +1,92 @@
+using Gtk;
 using System;
+using Transport;
 
 public class Dagatal
 {
     public static void Main()
     {
-        Logik logik = new Logik();
+        Vidmot vidmot = new Vidmot();
     }
 }
 public class Vidmot
 {
+    Window win;
+    VBox vb;
+    ScrolledWindow sw;
+    TextView tv;
+    Calendar cal;
+    TextBuffer tvBuffer;
+    string tvStrengur;
+    
+    public Vidmot()
+    {
+        Application.Init();
+        Logik logik = new Logik();
+
+        win = new Window("Diary");
+        vb = new VBox();
+        sw = new ScrolledWindow();
+        tv = new TextView();
+        cal = new Calendar();
+        tvBuffer = tv.Buffer;
+
+        tvBuffer.Text = logik.fetchCurrString();
+
+        sw.Add(tv);
+        vb.Add(cal);
+        vb.Add(sw);
+        win.Add(vb);
+
+        win.ShowAll();
+
+        win.Hidden += delegate
+        {
+            Application.Quit();
+        };
+
+        cal.DaySelected += delegate
+        {
+            string newDate = cal.GetDate().ToString("yyyyMMdd");
+            System.Console.WriteLine("Strengurinn var: " + tvBuffer.Text);
+            logik.loadDay(newDate, tvBuffer.Text);
+            tvBuffer.Text = logik.fetchCurrString();
+            System.Console.WriteLine("Nú er dagurinn: " + logik.fetchCurrDay());
+            System.Console.WriteLine("Nú eru skilaboðin: " + logik.fetchCurrString());
+        };
+
+        Application.Run();
+    }
+
+
 }
 
 public class Logik
 {
     private string currDay, currString, filename;
+    Value days;
 
     public Logik()
     {
         currDay = DateTime.Now.ToString("yyyyMMdd");
         filename = "calendardb.dat";
         currString = "";
-        loadDay(currDay);
+        days = Value.LoadFile(filename);
     }
 
-    void closeDay()
-    {
-        // update database
-        fileCheck();
-        days[currDay] = currString;
-    }
-
-    void loadDay(string dagur)
+    public void loadDay(string dagur, string exString)
     {
         // Retrieve from database
-        closeDay();
+        days[currDay] = exString;
         currDay = dagur;
-        Value days = Value.MakeTable();
-        days = Value.LoadFile(filename);
         try
-          {
-          currString = (string) days[currDay];
-          } catch(NullReferenceException e) 
-          {
-          currString = "";
-          }
+        {
+            currString = (string) days[currDay];
+        } catch(NullReferenceException e) 
+        {
+            currString = "";
+        }
     }
-
-    /*void switchDay(string dagur)
-      {
-    // Close current day and load a new one.
-    closeDay();
-    currString = loadDay(dagur);
-    currDay = dagur;
-    }*/
 
     public string fetchCurrDay()
     {
@@ -64,22 +98,5 @@ public class Logik
     {
         // Return the text of the selected day of in the logic part.
         return currString;
-    }
-
-    void fileCheck()
-    {
-        // Tests if calendar entry file exists and creates and initializes one
-        // if needed.
-        Value days;
-        try
-        {
-            days = Value.LoadFile(filename);
-        } catch(System.IO.FileNotFoundException e) {
-            System.IO.File.Create(filename);
-        } catch(System.IO.EndOfStreamException f) {
-            days = Value.MakeTable();
-            days[currDay] = "";
-            Value.SaveFile(days, filename);
-        }
     }
 }
